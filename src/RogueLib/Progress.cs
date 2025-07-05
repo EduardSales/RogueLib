3f338ba6d11b90145baefbbde342498f8e2ae315
Eduard Sales <50667613+EduardSales@users.noreply.github.com>
2025-07-05 16:59:05 +0200

    Add files via upload

---
+++ src/RogueLib/Progress.cs
@@ -0,0 +1,22 @@
+﻿using System.Runtime.InteropServices;
+
+namespace RogueLib
+{
+    /// <summary>
+    /// Represents a record of the player's progress choices,
+    /// acting as a log of the path taken through the game.
+    /// </summary>
+    public struct Progress
+    {
+        public string description;
+
+        public Progress(string _description)
+        {
+            description = _description;
+        }
+        public string GetProgress()
+        {
+            return description;
+        }
+    }
+}
